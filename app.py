fc520c0f5022b8c92f37f42414bb255ed36258c0
Saranyaraj <saranyaraj@purposeadvisorsolutions.com>
2024-06-02 20:20:40 -0500

    feat: integrate image capture and bot action into the main app thereby       closing most of the development work

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 from langchain_core.messages import HumanMessage
 import streamlit as st
 
-from components import executor
-
+from components import (
+    executor,
+    upload_image,
+)
 # st.image("./image/logo.webp", width=400)
 st.title("AI Recycling Assistant 🤖♻️")
 
-if prompt := st.chat_input():
-    st.chat_message("user").write(prompt)
+if img_file_buffer := st.camera_input("Take a picture for the AI Assistant to help you with recycling"):
+    url = upload_image(img_file_buffer)
+    st.chat_message("user").write(url)
     with st.chat_message("assistant"):
         st_callback = StreamlitCallbackHandler(st.container())
         human_message = HumanMessage(content=[
             { "type": "text", "text": "Can I recycle it? Also any additional information if available would be helpful" },
-            { "type": "image_url", "image_url": { "url": prompt }}
+            { "type": "image_url", "image_url": { "url": url }}
         ])
         response = executor.invoke({"input": human_message}, {"callbacks": [st_callback]})
         st.write(response["output"])
--- app_test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-from components import upload_image
-
-img_file_buffer = st.camera_input("Take a picture")
-
-if img_file_buffer is not None:
-    url = upload_image(img_file_buffer)
-    st.image(url, use_column_width=True)
